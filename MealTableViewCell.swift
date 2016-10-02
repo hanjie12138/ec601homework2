5c5c73bd1f91f03918a4b2d4805481fc56ecff25
Hanjie <hanjie@HanjiedeMacBook-Pro.local>
2016-10-02 14:40:27 -0400

    navigation

--- MealTableViewCell.swift
+++ MealTableViewCell.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class MealTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -20,5 +26,6 @@ class MealTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+
 
 }
