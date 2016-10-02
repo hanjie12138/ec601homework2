765322907218b4083ed52b3bba2d121ce8368b86
Hanjie <hanjie@HanjiedeMacBook-Pro.local>
2016-10-02 14:40:49 -0400

    navigation

--- MEAL.swift
+++ MEAL.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2016 Hanjie. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Meal {
+    // MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+}
