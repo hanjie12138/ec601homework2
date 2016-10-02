1d61ab092fd91221492a95ef6d6048c37ef95c13
Hanjie <hanjie@HanjiedeMacBook-Pro.local>
2016-10-02 13:39:13 -0400

    table view

---
+++ MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  foodtracker
+//
+//  Created by Hanjie on 10/2/16.
+//  Copyright © 2016 Hanjie. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
